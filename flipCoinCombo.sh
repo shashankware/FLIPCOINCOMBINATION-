e34114b33c849a9fe7c91dae07993aa34afd4371
shashank <shashank.w1998@gmail.com>
2021-02-20 15:08:29 +0530

    loopmul

--- flipCoinCombo.sh
+++ flipCoinCombo.sh
@@ -9,3 +9,23 @@ else
    echo "tail"
 fi
 
+#!/bin/bash
+declare -A dict
+headcount=0
+tailcount=0
+for ((i=1;i<=20;i++))
+do
+   randomNumber=$(($RANDOM%2))
+   if [ $randomNumber -eq 1 ]
+   then
+      headcount=$(($headcount+1))
+   else
+      tailcount=$(($tailcount+1))
+   fi
+done
+head_percentage=$(($headcount*5))
+tail_percentage=$(($tailcount*5))
+dict[H]=$head_percentage
+dict[T]=$tail_percentage
+
+
